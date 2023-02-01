62ccf1d2d4af974cc5ef4845260cce0cd8b59c9f
Kish <kisha_simpson@yahoo.com>
2023-02-01 00:00:25 -0600

    search

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,25 +11,76 @@ class BinarySearchTree {
 
   constructor() {
     this.root = null;
-    this.left = null;
-    this.right = null;
   }
 
   insert(val, currentNode = this.root) {
+    let newNode = new TreeNode(val)
     if (!this.root) {
-      currentNode = new TreeNode(val);
-      console.log(currentNode)
+      this.root = newNode;
     } else if (val < currentNode.val) {
-      currentNode.left = TreeNode(val);
+      if (!currentNode.left) {
+        currentNode.left = newNode
+        return
+      } else {
+        this.insert(val, currentNode.left)
+      }
+
     } else {
-      currentNode.right = TreeNode(val);
+      if (!currentNode.right) {
+        currentNode.right = newNode
+        return
+      } else {
+        this.insert(val, currentNode.right)
+      }
     }
 
+    // iteration
+  //   while(currentNode) {
+  //    if (val < currentNode.val) {
+  //       if (!currentNode.left) {
+  //         currentNode.left = newNode
+  //         return
+  //       } else {
+  //         currentNode = currentNode.left
+  //       }
+  //     }
+  //     if (val > currentNode.val) {
+  //       if (!currentNode.right) {
+  //         currentNode.right = newNode
+  //       return
+  //     } else {
+  //       currentNode = currentNode.right
+  //     }
+  //   }
+  // }
+}
+
+  search(val, currentNode = this.root) {
+   if (!currentNode) return false
+
+   //recursive
+  //  if (currentNode.val === val) return true
+  //  if (val < currentNode.val) return this.search(val, currentNode.left)
+  //  if (val > currentNode.val) return this.search(val, currentNode.right)
+
+
+  // iteration
+    while(currentNode) {
+      if (currentNode.val === val) return true
+      if (val < currentNode.val) {
+        if (!currentNode.left) return false;
+        else currentNode = currentNode.left
+      }
+      if (val > currentNode.val) {
+        if (!currentNode.left) return false;
+        else currentNode = currentNode.right
+      }
+    }
   }
 
-  search(val) {
-    // Your code here
-  }
+
+
+
 
 
   preOrderTraversal(currentNode = this.root) {
@@ -59,10 +110,22 @@ class BinarySearchTree {
 
 
 
-// bst = new BinarySearchTree();
+bst = new BinarySearchTree();
 
-// console.log(bst.insert(4));
-// console.log(bst.insert(2));
+bst.insert(4);
+bst.insert(2);
+bst.insert(6);
+bst.insert(1);
+bst.insert(3);
+bst.insert(5);
+bst.insert(7);
 
+// bst.search(1)
+// bst.search(2)
+// bst.search(3)
+// bst.search(4)
+// bst.search(5)
+// bst.search(6)
+bst.search(7)
 
 module.exports = { BinarySearchTree, TreeNode };
